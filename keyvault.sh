a24d45647ab1bccc03009363d31082860898671c
Massimo Consiglio <massimo.consiglio@dwp.gov.uk>
2020-02-07 14:05:15 +0000

    Added service principal

--- keyvault.sh
+++ keyvault.sh
@@ -3,6 +3,8 @@ CLIENTSECRET=2c24aa11-ab58-4356-9548-db3bfa8331b5
 SQLUSR=sqladmin9Cv6941
 SQLPWD=pC3kn9T85
 KV_NAME=aks010kv
+SUBSCRIPTION=ae235c5e-7998-4b1d-9a67-40dfcab3f663
+RES_GROUP=teamResources
 
 #Create key vault
 az keyvault create -n aks010kv -g teamResources --sku premium
@@ -15,7 +17,12 @@ kubectl get pods -n kv
 #Should be 3 pods
 
 #Add your service principal credentials as Kubernetes secrets accessible by the Key Vault FlexVolume driver
-kubectl create secret generic cluspncreds --from-literal clientid=$CLIENTID --from-literal clientsecret=$CLIENTSECRET --type=azure/kv --namespace api-dev
+kubectl create secret generic cluspncreds --from-literal clientid=$CLIENTID --from-literal \
+clientsecret=$CLIENTSECRET --type=azure/kv --namespace api-dev
+
+# [Required for version < v0.0.13] Assign Reader Role to the service principal for your keyvault
+az role assignment create --role Reader --assignee $CLIENTID \
+--scope /subscriptions/$SUBSCRIPTION/resourcegroups/$RES_GROUP/providers/Microsoft.KeyVault/vaults/$KV_NAME
 
 # Assign key vault permissions to service principal
 az keyvault set-policy -n $KV_NAME --key-permissions get --spn $CLIENTID
